82e3dccfe4258eb631b425f241ee816ee1ff3db5
PaulSeiferAB <paul.seifer@autobrains.ai>
2023-12-12 13:17:41 +0200

    added metrics for pilot light and cpu per core

--- gpumon.py
+++ gpumon.py
@@ -14,17 +14,32 @@
 # Some changes to script have been made by Paul Seifer to adapt to python3.9, such as conversion of values to utf-8 strings.
 
 import urllib.request as urllib2
+import psutil
 import boto3
 from pynvml import *
-from datetime import datetime
+from datetime import datetime, timedelta
 from time import sleep
+
+# Constants
+CACHE_DURATION = 300  # 5 minutes in seconds
+THRESHOLD_PERCENTAGE = 10  # Threshold for average core utilization
+
+# Variables
+core_utilization_cache = [[] for _ in range(psutil.cpu_count())]  # List to store utilization data for each core
+cpu_util_tripped = 0
+
+def get_per_core_cpu_utilization():
+    return psutil.cpu_percent(interval=1, percpu=True)
+
+def calculate_average_core_utilization():
+    return [sum(core_data) / len(core_data) if core_data else 0 for core_data in core_utilization_cache]
 
 ### CHOOSE REGION ####
 EC2_REGION = 'eu-west-1'
 
 ###CHOOSE NAMESPACE PARMETERS HERE###
 my_NameSpace = 'GPU-metrics-with-team-tag' 
-
+#my_NameSpace = 'Alarm-new-namespace'
 ### CHOOSE PUSH INTERVAL ####
 sleep_interval = 10
 
@@ -99,11 +114,12 @@ def getUtilization(handle):
         PUSH_TO_CW = False
     return util, gpu_util, mem_util
 
-def logResults(team, emp_name, i, util, gpu_util, mem_util, powDrawStr, temp):
+def logResults(team, emp_name, i, util, gpu_util, mem_util, powDrawStr, temp, average_gpu_util,alarm_pilot_light,cpu_util_tripped):
     try:
         gpu_logs = open(TMP_FILE_SAVED, 'a+')
-        writeString = 'tag:' + team + ',' + 'Employee:' + emp_name + ',' + str(i) + ',' + gpu_util + ',' + mem_util + ',' + powDrawStr + ',' + temp + '\n'
+        writeString = 'tag:' + team + ',' + 'Employee:' + emp_name + ',' + 'GPU_ID:' + str(i) + ',' + 'GPU_Util:' + gpu_util + ',' + 'MemUtil:' + mem_util + ',' + 'powDrawStr:' + powDrawStr + ',' + 'Temp:' + temp + ',' + 'AverageGPUUtil:' + str(average_gpu_util) + ',' + 'Alarm_Pilot_value:' + str(alarm_pilot_light) + ',' + 'CPU_Util_Tripped:' + str(cpu_util_tripped) + '\n'
         #print(writeString)
+        #writeString = 'tag:' + team + ',' + 'Employee:' + emp_name + ',' + str(i) + ',' + gpu_util + ',' + mem_util + ',' + powDrawStr + ',' + temp + '\n'
         gpu_logs.write(writeString)
     except:
         print("Error writing to file ", gpu_logs)
@@ -135,6 +151,14 @@ def logResults(team, emp_name, i, util, gpu_util, mem_util, powDrawStr, temp):
                         'Name': 'EmployeeTag',
                         'Value': str(emp_name)
                     }
+                 #   {
+                 #       'Name': 'Average GPU Utilization',
+                 #       'Value': str(average_gpu_util)
+                 #   }
+                 #   {
+                 #       'Name': 'Alarm_pilot_light',
+                 #       'Value': str(alarm_pilot_light)
+                 #   }
 
                 ]
         cloudwatch.put_metric_data(
@@ -166,7 +190,29 @@ def logResults(team, emp_name, i, util, gpu_util, mem_util, powDrawStr, temp):
                     'Unit': 'None',
                     'StorageResolution': store_reso,
                     'Value': int(temp)
-                },            
+                },
+                {
+                    'MetricName': 'Alarm Pilot Light (1/0)',
+                    'Dimensions': MY_DIMENSIONS,
+                    'Unit': 'None',
+                    'StorageResolution': store_reso,
+                    'Value': float(alarm_pilot_light)
+                },
+                {
+                    'MetricName': 'Average GPU Utilization',
+                    'Dimensions': MY_DIMENSIONS,
+                    'Unit': 'Percent',
+                    'StorageResolution': store_reso,
+                    'Value': float(average_gpu_util)
+                },
+                {
+                    'MetricName': 'CPU Utilization Low Tripped',
+                    'Dimensions': MY_DIMENSIONS,
+                    'Unit': 'None',
+                    'StorageResolution': store_reso,
+                    'Value': float(cpu_util_tripped)
+                }
+
         ],
             Namespace=my_NameSpace
         )
@@ -174,10 +220,35 @@ def logResults(team, emp_name, i, util, gpu_util, mem_util, powDrawStr, temp):
 
 nvmlInit()
 deviceCount = nvmlDeviceGetCount()
-
 def main():
+    global core_utilization_cache
+    alarm_pilot_light = 0
+    cpu_util_tripped = False
     try:
         while True:
+            total_gpu_util = 0
+            try:
+                # Get per-core CPU utilization
+                per_core_utilization = get_per_core_cpu_utilization()
+
+                # Cache utilization data for each core
+                for i, core_util in enumerate(per_core_utilization):
+                    core_utilization_cache[i].append(core_util)
+
+                # Remove outdated data from the cache
+                current_time = datetime.now()
+                core_utilization_cache = [core_data[-int(CACHE_DURATION / 1):] for core_data in core_utilization_cache]
+
+                # Calculate average core utilization
+                average_core_utilization = calculate_average_core_utilization()
+                #print(average_core_utilization)
+                # Check if any core exceeds the threshold
+                if any(utilization > THRESHOLD_PERCENTAGE for utilization in average_core_utilization):
+                    cpu_util_tripped = True
+                    #print("Threshold exceeded. Changing variable to True.")
+            except:
+                    print('Could not get cpu core utilization statistics, debug')
+
             tags = get_instance_tags(INSTANCE_ID)
             if 'Team' in tags:
                 team = str(tags['Team'])
@@ -195,10 +266,27 @@ def main():
                 powDrawStr = getPowerDraw(handle)
                 temp = getTemp(handle)
                 util, gpu_util, mem_util = getUtilization(handle)
-                logResults(team, emp_name, i, util, gpu_util, mem_util, powDrawStr, temp)
+                total_gpu_util += float(gpu_util)
+                #logResults(team, emp_name, i, util, gpu_util, mem_util, powDrawStr, temp, average_gpu_util)
 
+            average_gpu_util = total_gpu_util / deviceCount
+            
+            #print(f'Average GPU utilization:',average_gpu_util)
+            if round(float(average_gpu_util)) <= 10 and cpu_util_tripped == False:    
+                if alarm_pilot_light == 0:
+                    #print(f'CPU or GPU below {THRESHOLD_PERCENTAGE}% threshold, turning pilot light ON')
+                    alarm_pilot_light = 1
+            else:
+                if alarm_pilot_light == 1:
+                    alarm_pilot_light = 0
+                    #print(f'CPU or GPU above {THRESHOLD_PERCENTAGE}% threshold, turning pilot light OFF')
+            
+            # Log the results
+            for i in range(deviceCount):
+                handle = nvmlDeviceGetHandleByIndex(i)
+                logResults(team, emp_name, i, util, gpu_util, mem_util, powDrawStr, temp, average_gpu_util, alarm_pilot_light,cpu_util_tripped)
+            
             sleep(sleep_interval)
-
     finally:
         nvmlShutdown()
 
